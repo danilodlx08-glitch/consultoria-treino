be33a97a0d48cdc1aa41ac70c50a140e10f09371
danilodlx08-glitch <danilodlx08@gmail.com>
2026-09-13 22:25:47 -0300

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = https://pbtmbgcilxwckpbmcutx.supabase.co
-const supabaseKey = sb_publishable_SR_Rn6n8t44rNjwsgZi4tQ_drRvCPLk
+const supabaseUrl = 'https://pbtmbgcilxwckpbmcutx.supabase.co'
+const supabaseKey = 'sb_publishable_SR_Rn6n8t44rNjwsgZi4tQ_drRvCPLk'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
